359dd43063abc2f063d78bdc5ae19afa5fa04b79
Arnab <arnabdnri@gmail.com>
2022-06-13 10:13:17 +0800

    Add function for single game

--- r-p-s.js
+++ r-p-s.js
@@ -11,4 +11,38 @@ function getRandomNum(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "rock") {
+        switch (computerSelection) {
+            case "rock": 
+                return "It's a draw!";
+            case "paper": 
+                return "You lose, paper beats rock!";
+            case "scissor": 
+                return "You win, rock beats scissor!";
+        }
+    } else if (playerSelection == "paper") {
+        switch (computerSelection) {
+            case "paper": 
+                return "It's a draw!";
+            case "scissor": 
+                return "You lose, scissor beats paper!";
+            case "rock": 
+                return "You win, paper beats rock!";
+        }
+    } else if (playerSelection == "scissor") {
+        switch (computerSelection) {
+            case "scissor": 
+                return "It's a draw!";
+            case "paper": 
+                return "You win, scissor beats paper!";
+            case "rock": 
+                return "You lose, rock beats scissor!";
+        }
+    } else return "Please enter a valid play.";
+}
+
+const playerSelection = "rock".toLowerCase();
+const computerSelection = computerPlay().toLowerCase();
+
+console.log(playRound(playerSelection, computerSelection));
